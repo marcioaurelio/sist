29bf0a8f4744c59155d79f67c2022a1ca913ab76
Márcio Aurélio Tapajós Araújo <marcio.araujo@webmail.susipe.pa.gov.br>
2019-09-23 12:07:40 -0300

    comentando rota reset de senha!

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,21 @@ Route::get('/', function () {
 
 });
 
-\Auth::routes();
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('admin')->group (function(){
-    Route::get('admin', function(){
-        ('Auth\LoginController@showLoginForm')->name('admin');
-    });
+Route::group(['prefix' => 'admin'], function() {
 
     Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
     Route::post('login', 'Auth\LoginController@login');
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+    /*Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');*/
+
 });
 
 
