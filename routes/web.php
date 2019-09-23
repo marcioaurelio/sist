86f095ac085bde848f6d7657dd3d078047138552
Márcio Aurélio Tapajós Araújo <marcio.araujo@webmail.susipe.pa.gov.br>
2019-09-23 12:12:06 -0300

    comentando rota reset de senha!

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
     Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
     Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');*/
+    //router
 
 });
 
