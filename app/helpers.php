e7d482ca2e346d306276235e816195dbe23076e8
Márcio Aurélio Tapajós Araújo <marcio.araujo@webmail.susipe.pa.gov.br>
2019-09-24 11:35:55 -0300

    modificação do nome da rota! criação da pasta admin dentro da wiew, arquivos login.blade e welcome.blade. modificação do arquivo web.php adicionado rota para /admin.

--- app/helpers.php
+++ app/helpers.php
@@ -2,3 +2,8 @@
 function layout(){
 return \Request::is('admin/*') ? 'layouts.admin':'layouts.app';
 }
+
+function isAdmin(){
+    return \Request::is('admin/*') ? true:false;
+
+};
--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,25 @@ Route::get('/', function () {
 
 });
 
+Route::get('/admin', function () {
+    return view('admin.welcome');
+
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin'], function(){
 
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('admin.login');
     Route::post('login', 'Auth\LoginController@login');
-    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    Route::post('logout', 'Auth\LoginController@logout')->name('admin.logout');
 
     /*Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
     Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
     Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');*/
-    //router
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+    Route::get('/home', 'HomeController@index')->name('home');*/
 
 });
 
